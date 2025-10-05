3d7c725f7f3ecbd3884ae558f0ba90666beb675d
Christian Isai <christian.isai@1erotrader.com>
2025-10-05 01:25:29 -0600

    feat: Add unit tests for MovieRepositoryImpl with mock implementations

--- test/mocks.mocks.dart
+++ test/mocks.mocks.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:mockito/annotations.dart';
 import 'package:black_bull/core/services/local_storage_service.dart';
 
 @GenerateMocks([LocalStorageService])
+@GenerateMocks([Dio])
 void main() {}
---
+++ test/movie_repository_impl_test.dart
@@ -0,0 +1,236 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:black_bull/core/error/failure.dart';
+
+import 'package:black_bull/domain/entities/movie_entity.dart';
+import 'package:black_bull/domain/entities/popular_movies_entity.dart';
+import 'package:black_bull/domain/entities/search_response_entity.dart';
+import 'package:black_bull/data/models/popular_movies_model.dart';
+import 'package:black_bull/data/models/search_response_model.dart';
+import 'package:black_bull/data/models/movie_detail_model.dart';
+import 'package:black_bull/data/repositories/movies_repository_imp.dart';
+
+import './mocks.mocks.dart';
+import 'mocks.mocks.mocks.dart';
+
+class FakePopularMoviesResponseModel extends PopularMoviesResponseModel {
+  FakePopularMoviesResponseModel()
+    : super(page: 1, results: [], totalPages: 1, totalResults: 0);
+
+  @override
+  PopularMoviesResponseEntity toEntity() => PopularMoviesResponseEntity(
+    page: 1,
+    results: [],
+    totalPages: 1,
+    totalResults: 0,
+  );
+}
+
+class FakeSearchResponseModel extends SearchResponseModel {
+  FakeSearchResponseModel()
+    : super(page: 1, results: [], totalPages: 1, totalResults: 0);
+
+  @override
+  SearchResponseEntity toEntity() => SearchResponseEntity(
+    page: 1,
+    results: [],
+    totalPages: 1,
+    totalResults: 0,
+  );
+}
+
+class FakeMovieDetail extends MovieDetail {
+  FakeMovieDetail()
+    : super(
+        id: 1,
+        title: "Fake Movie",
+        adult: false,
+        budget: 0,
+        genres: [],
+        homepage: '',
+        overview: '',
+        popularity: 0.0,
+        posterPath: '',
+        releaseDate: '',
+        revenue: 0,
+        runtime: 0,
+        status: '',
+        tagline: '',
+        voteAverage: 0.0,
+        voteCount: 0,
+        imdbId: '',
+        originalLanguage: 'es',
+        originalTitle: 'Fake Movie',
+        productionCompanies: [],
+        productionCountries: [],
+        spokenLanguages: [],
+        video: false,
+      );
+
+  @override
+  MovieEntity toEntity() => MovieEntity(
+    id: 1,
+    title: "Fake Movie",
+    posterPath: '',
+    releaseDate: '',
+    synopsis: '',
+    rating: 0.0,
+    genres: [],
+  );
+}
+
+void main() {
+  late MovieRepositoryImpl repository;
+  late MockDio mockDio;
+
+  setUp(() {
+    mockDio = MockDio();
+    repository = MovieRepositoryImpl(mockDio);
+  });
+
+  group('getPopularMovies', () {
+    test('should return PopularMoviesResponseEntity on success', () async {
+      final fakeJson = {
+        'page': 1,
+        'results': [],
+        'total_pages': 1,
+        'total_results': 0,
+      };
+      when(
+        mockDio.get(any, queryParameters: anyNamed('queryParameters')),
+      ).thenAnswer(
+        (_) async => Response(
+          data: fakeJson,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+
+      final result = await repository.getPopularMovies(page: 1);
+
+      expect(result.isRight(), true);
+      result.fold((_) => fail('Should not fail'), (entity) {
+        expect(entity, isA<PopularMoviesResponseEntity>());
+      });
+    });
+
+    test('should return Failure on Dio error', () async {
+      when(
+        mockDio.get(any, queryParameters: anyNamed('queryParameters')),
+      ).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(path: '/movie/popular'),
+          type: DioExceptionType.connectionTimeout,
+        ),
+      );
+
+      final result = await repository.getPopularMovies(page: 1);
+
+      expect(result.isLeft(), true);
+    });
+  });
+
+  group('searchMovies', () {
+    test('should return SearchResponseEntity on success', () async {
+      final fakeJson = {
+        'page': 1,
+        'results': [],
+        'total_pages': 1,
+        'total_results': 0,
+      };
+      when(
+        mockDio.get(any, queryParameters: anyNamed('queryParameters')),
+      ).thenAnswer(
+        (_) async => Response(
+          data: fakeJson,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+
+      final result = await repository.searchMovies(query: 'test', page: 1);
+
+      expect(result.isRight(), true);
+      result.fold((_) => fail('Should not fail'), (entity) {
+        expect(entity, isA<SearchResponseEntity>());
+      });
+    });
+
+    test('should return Failure on Dio error', () async {
+      when(
+        mockDio.get(any, queryParameters: anyNamed('queryParameters')),
+      ).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(path: '/search/movie'),
+          type: DioExceptionType.receiveTimeout,
+        ),
+      );
+
+      final result = await repository.searchMovies(query: 'fail', page: 1);
+
+      expect(result.isLeft(), true);
+    });
+  });
+
+  group('getMovie', () {
+    test('should return MovieEntity on success', () async {
+      final fakeJson = {
+        'adult': true,
+        'backdrop_path': 'backdropPath',
+        'belongs_to_collection': 'belongsToCollection',
+        'budget': 'budget',
+        'genres': [],
+        'homepage': 'homepage',
+        'id': 1,
+        'imdb_id': 'imdbId',
+        'original_language': 'originalLanguage',
+        'original_title': 'originalTitle',
+        'overview': 'overview',
+        'popularity': 5.5,
+        'poster_path': 'posterPath',
+        'production_companies': [],
+        'production_countries': [],
+        'release_date': '1990',
+        'revenue': 'revenue',
+        'runtime': 'runtime',
+        'spoken_languages': [],
+        'status': 'status',
+        'tagline': 'tagline',
+        'title': 'title',
+        'video': true,
+        'vote_average': 5.5,
+        'vote_count': 100,
+      };
+      when(mockDio.get(any)).thenAnswer(
+        (_) async => Response(
+          data: fakeJson,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+
+      final result = await repository.getMovie(id: 1);
+
+      expect(result.isRight(), true);
+      result.fold((_) => fail('Should not fail'), (entity) {
+        expect(entity, isA<MovieEntity>());
+        expect(entity.id, 1);
+      });
+    });
+
+    test('should return Failure when Dio throws', () async {
+      when(mockDio.get(any)).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(path: '/movie/1'),
+          type: DioExceptionType.badResponse,
+        ),
+      );
+
+      final result = await repository.getMovie(id: 1);
+
+      expect(result.isLeft(), true);
+    });
+  });
+}
